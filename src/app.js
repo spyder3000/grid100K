7570b444716aa55c15e3aed819202c4cc0f82193
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2021-04-20 23:25:56 -0400

    Misc mods

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cors()); 
 
 // DEfine paths for Express config;  __dirname is path to current directory;  path.join to go up one level & into public dir;  
-const publicDirectoryPath = path.join(__dirname, '../public');  // this line will match to public files first (e.g. index.html) prior to app.get stmts below  
+// JV   const publicDirectoryPath = path.join(__dirname, '../public');  // this line will match to public files first (e.g. index.html) prior to app.get stmts below  
 // const partialsPath = path.join(__dirname, '../templates/partials'); 
 //const viewsPath = path.join(__dirname, '../templates/views');  // express defaults to 'views' folder;  this modifies that to 'templates/views' instead 
 
@@ -19,7 +19,8 @@ const publicDirectoryPath = path.join(__dirname, '../public');  // this line wil
 // app.set('view engine', 'hbs');     // e.g. set up a view engine (handlebar) for Express 
 //app.set('views', viewsPath);   // express default is 'views' folder for .hbs content;  this overrides that  
 // hbs.registerPartials(partialsPath);  
-app.use(express.static(publicDirectoryPath))   // Setup static directory;  app.use to customize our server;  
+
+// JV  app.use(express.static(publicDirectoryPath))   // Setup static directory;  app.use to customize our server;  
 
 /* functions provided by express */
 // express.json() -- setup express to automatically parse incoming json (e.g. from POST) to an object so we can access in 
@@ -31,11 +32,10 @@ app.use('/api/', personRouter);
 console.log('process.env.NODE_ENV = ' + process.env.NODE_ENV); 
 if (process.env.NODE_ENV === 'production') {
    // Set static folder 
-   app.use(express.static('client/public')); 
+   app.use(express.static('client\public')); 
    app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html')); 
    })
 }
-app.use('/') 
 
 module.exports = app
